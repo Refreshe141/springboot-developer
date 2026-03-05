f8c5e2a21d96c3c4a5d0e62e5cfb6d083a6b9f9c
kimhyeok <minhyeok4087@gmail.com>
2026-03-05 12:05:42 +0900

    챕터3

---
+++ src/main/java/com/kmh/Spring01/Student.java
@@ -0,0 +1,16 @@
+package com.kmh.Spring01;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    public Student (String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName  = lastName;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+}
