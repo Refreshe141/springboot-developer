184a708ade23b79177275e0ffefc6f98a23d48e7
kimhyeok <minhyeok4087@gmail.com>
2026-03-24 16:14:47 +0900

    빌드 그레들 수정 밑 콘트롤러 추가

--- src/main/java/com/kmh/Spring01/TestController.java
+++ src/main/java/com/kmh/Spring01/TestController.java
@@ -1,24 +1,15 @@
 package com.kmh.Spring01;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 @RestController
-//모든 메서드가 데이터를 리턴한다
-//@Controller //기본적으로 메서드 리턴하는 것이 HTML 파일이름이다
 public class TestController {
-    @Autowired
-    private TestService testService;
 
-    @GetMapping("/test")
-    //@ResponseBody
-    public ResponseEntity<List<Member>> getAllMembers(){
-        return ResponseEntity.ok(testService.getAllMembers());
-    }
     @PostMapping("/test")
-    public ResponseEntity<Member> createMember(@RequestBody Member member){
-        return ResponseEntity.ok(testService.saveMember(member));
+    public ResponseEntity<String> test() {
+        return new ResponseEntity<>("Hello world", HttpStatus.CREATED);
     }
 }
--- src/test/java/com/kmh/Spring01/TestControllerTest.java
+++ src/test/java/com/kmh/Spring01/TestControllerTest.java
@@ -1,58 +1,40 @@
 package com.kmh.Spring01;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.bind.annotation.PostMapping;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class TestControllerTest {
+
     @Autowired
     protected MockMvc mockMvc;
 
-    @Autowired
-    private WebApplicationContext context;
-
-    @Autowired
-    private TestRepository testRepository;
-
-    @BeforeEach
-    public void mockMvcSetup(){
-        //실제 스프링 웹 컨텍스트를 사용해 MockMvc 테스트 환경을 초기화한다
-        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
-    }
-
-
-    @DisplayName("getAllMembers: 맴버 조회에 성공.")
     @Test
-    public void getAllMembers() throws Exception {
-        // given
-        Member savedMember = testRepository.save(new Member("홍길동"));
+    @DisplayName("GET /test 요청 시 Hello world 반환")
+    void getTestAPI() throws Exception {
 
-        // when
-        final ResultActions result = mockMvc.perform(get("/test")
-                .accept(MediaType.APPLICATION_JSON));
-
-        // then
-        result.andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].id").value(savedMember.getId()))
-                .andExpect(jsonPath("$[0].name").value(savedMember.getName()));
+        mockMvc.perform(post("/test"))
+                .andExpect(status().isCreated())
+                .andExpect(content().string("Hello world"))
+                .andDo(print());
     }
-    @AfterEach
-    public void cleanUp(){
-        testRepository.deleteAll();
+
+    @PostMapping("test")
+    public ResponseEntity<String> test(){
+        return new ResponseEntity<String>("Hello world", HttpStatus.CREATED);
     }
 }
