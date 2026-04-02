a8a1a0ba67712c661125458280aeb1fe158ef3ad
kimhyeok <minhyeok4087@gmail.com>
2026-04-02 12:48:55 +0900

    init:기존파일 삭제 및 레이어분리 패키지 추가

---
+++ src/main/java/com/kmh/Spring01/dao/Article.java
@@ -0,0 +1,17 @@
+package com.kmh.Spring01.dao;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Article {
+
+    /*
+        id(bigint, not bull, primary key)
+        title(varchar(255), not null)
+        content (varchar(255), not null)
+     */
+}
