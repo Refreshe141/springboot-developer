37b18d0b45c32518a72c62cc2a7e3538b0fa0bb4
kimhyeok <minhyeok4087@gmail.com>
2026-04-02 12:23:41 +0900

    init:crud테스트 실습 코드 추가

--- src/main/java/com/kmh/Spring01/Member.java
+++ src/main/java/com/kmh/Spring01/Member.java
@@ -18,6 +18,11 @@ import lombok.NoArgsConstructor;
         @Column(name = "name", nullable = false)
         private String name;
 
+
+        public void changeName(String name){
+            this.name = name;
+        }
+
         public Member(String name){
             this.name = name;
         }
--- src/main/java/com/kmh/Spring01/TestRepository.java
+++ src/main/java/com/kmh/Spring01/TestRepository.java
@@ -3,7 +3,11 @@ package com.kmh.Spring01;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TestRepository extends JpaRepository<Member,Long> {
     Long id(Long id);
+
+    public List<Member>findByName(String name);
 }
