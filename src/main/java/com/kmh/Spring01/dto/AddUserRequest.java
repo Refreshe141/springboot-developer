e6f1d8138d6365c9bbb9311ac12b490c56ce5cd0
2201320김민혁 <minhyeok4087@gmail.com>
2026-06-04 12:17:09 +0900

    로그인 로그아웃 회원가입 구현 스프링 시큐리티

---
+++ src/main/java/com/kmh/Spring01/dto/AddUserRequest.java
@@ -0,0 +1,14 @@
+package com.kmh.Spring01.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AddUserRequest {
+    private String  email;
+    private String password;
+
+
+}
---
+++ src/main/java/com/kmh/Spring01/service/UserService.java
@@ -0,0 +1,22 @@
+package com.kmh.Spring01.service;
+
+import com.kmh.Spring01.domain.User;
+import com.kmh.Spring01.dto.AddUserRequest;
+import com.kmh.Spring01.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    public Long save(AddUserRequest dto) {
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .build()).getId();
+    }
+}
